5cd204f8b42d4148c71f24ea63f35d534de0c96f
chriszdeux <chriszdeux@gmail.com>
2022-05-13 15:27:24 -0500

    v2 build

---
+++ docs/precache-manifest.f76dfde3b8368267a34e51b83cb02683.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6a982d852eb8ac64ccae62bcb3cf96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69ed1654f335ac282ef9",
+    "url": "/static/css/2.1c3129d7.chunk.css"
+  },
+  {
+    "revision": "b4d073a74da351f159e0",
+    "url": "/static/css/main.f5ab71c7.chunk.css"
+  },
+  {
+    "revision": "69ed1654f335ac282ef9",
+    "url": "/static/js/2.d586eabf.chunk.js"
+  },
+  {
+    "revision": "925220e9e3aa57005a820a080e44208e",
+    "url": "/static/js/2.d586eabf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d073a74da351f159e0",
+    "url": "/static/js/main.c16de993.chunk.js"
+  },
+  {
+    "revision": "55999b852509ae98f7cd",
+    "url": "/static/js/runtime-main.ea39fafa.js"
+  },
+  {
+    "revision": "f7546da928da711568e86c3c9ef68fcd",
+    "url": "/static/media/asset-background.f7546da9.svg"
+  },
+  {
+    "revision": "ce552caa0bd5bf924330974313674a8f",
+    "url": "/static/media/chain.ce552caa.jpg"
+  },
+  {
+    "revision": "3dc8cd1cd975a167408f441b700dcef2",
+    "url": "/static/media/financial.3dc8cd1c.svg"
+  },
+  {
+    "revision": "a3855be12641f1c120906dfd295d59a6",
+    "url": "/static/media/grow.a3855be1.svg"
+  },
+  {
+    "revision": "bcd75de702addb6ae59224a95a88bd9a",
+    "url": "/static/media/kraken.bcd75de7.png"
+  },
+  {
+    "revision": "e1bcd62f60dccfa497564b7470b22d28",
+    "url": "/static/media/learn.e1bcd62f.svg"
+  },
+  {
+    "revision": "7abbe19a1c9193ecfd62faa407eb560c",
+    "url": "/static/media/news.7abbe19a.svg"
+  },
+  {
+    "revision": "ebab17dca450886b652977bd191e9fb3",
+    "url": "/static/media/nft.ebab17dc.jpeg"
+  },
+  {
+    "revision": "90428c22ee201460c72d91087f2108d6",
+    "url": "/static/media/payment.90428c22.svg"
+  },
+  {
+    "revision": "0b61293857d2cca473e88cfabfd006d0",
+    "url": "/static/media/server.0b612938.svg"
+  },
+  {
+    "revision": "8a546902c3768a52b38269ac57a0a8e8",
+    "url": "/static/media/study.8a546902.svg"
+  },
+  {
+    "revision": "543bb5b5381feb0883ab16de1fb28449",
+    "url": "/static/media/winner.543bb5b5.svg"
+  },
+  {
+    "revision": "fb7142cf3489b9257f2a1256dad8f9d3",
+    "url": "/static/media/world.fb7142cf.svg"
+  }
+]);
